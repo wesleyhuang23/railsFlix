dcbbf2f5d5559e28972bc3900d893fbf2f65f63e
Wesley <wesley@gooddogdesign.com>
2018-07-03 16:35:55 -0700

    detail page showing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,5 +2,10 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def detail
+    @movie = Movie.find(params[:id])
+    @actors = @movie.actors
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/movies' => 'movies#index'
+  get '/movies/:id' => 'movies#detail', as: :movie
 end
